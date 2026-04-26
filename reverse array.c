f5037a5a57ddebae35d4825b946d622f65a0fd16
daim-007 <daimsheikh49@gmail.com>
2026-04-26 22:00:46 +0530

    Add files via upload

---
+++ reverse array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print in reverse order
+    printf("Array in reverse:\n");
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
